8fc7ce18250e38a573cebfb60d70900ca90704bc
Jorge Rea <jorge.lrs497@gmail.com>
2019-04-16 00:01:31 -0500

    Correcciones

--- widgets/header.php
+++ widgets/header.php
@@ -6,21 +6,23 @@
     function getHeader($user){
         echo"<header class='row sa'>";
         getLogo();
-        if($_GET['status']==="error"){
+        if(isset($_GET['status'])){
+            if($_GET['status']=="error"){
             echo "<span class='superTostada show'>
                     Usuario Inexistente.
                  </span>";
             echo "<script>
                     setTimeout(function(){ document.querySelector('.superTostada').classList.remove('show'); }, 4500);
                 </script>";
-        }
-        elseif($_GET['status']==="errorExist"){
-            echo "<span class='superTostada show'>
-                    El Usuario Existe.
-                 </span>";
-            echo "<script>
-                    setTimeout(function(){ document.querySelector('.superTostada').classList.remove('show'); }, 4500);
-                </script>";
+            }
+            elseif($_GET['status']=="errorExist"){
+                echo "<span class='superTostada show'>
+                        El Usuario Existe.
+                     </span>";
+                echo "<script>
+                        setTimeout(function(){ document.querySelector('.superTostada').classList.remove('show'); }, 4500);
+                    </script>";
+            }
         }
         echo "<div id='menubtn'>Menu</div>";
         if(isset($user))
