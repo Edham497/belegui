593f87fef91b8ca230e10789bc24807e36801a55
Jorge Rea <jorge.lrs497@gmail.com>
2019-04-15 23:52:29 -0500

    mas correcciones por la version de php

--- widgets/header.php
+++ widgets/header.php
@@ -7,20 +7,22 @@
         echo"<header class='row sa'>";
         getLogo();
         if(isset($_GET['status'])){
+            if($_GET['status']=="error"){
             echo "<span class='superTostada show'>
                     Usuario Inexistente.
                  </span>";
             echo "<script>
                     setTimeout(function(){ document.querySelector('.superTostada').classList.remove('show'); }, 4500);
                 </script>";
-        }
-        elseif($_GET['status']==="errorExist"){
-            echo "<span class='superTostada show'>
-                    El Usuario Existe.
-                 </span>";
-            echo "<script>
-                    setTimeout(function(){ document.querySelector('.superTostada').classList.remove('show'); }, 4500);
-                </script>";
+            }
+            elseif($_GET['status']=="errorExist"){
+                echo "<span class='superTostada show'>
+                        El Usuario Existe.
+                     </span>";
+                echo "<script>
+                        setTimeout(function(){ document.querySelector('.superTostada').classList.remove('show'); }, 4500);
+                    </script>";
+            }
         }
         echo "<div id='menubtn'>Menu</div>";
         if(isset($user))
